560df5637a561a29d376ea2012ee1f65d4fa2509
Yarikua <mr.yarikua@gmail.com>
2017-04-08 17:02:06 +0200

    wprawie

--- js/main.js
+++ js/main.js
@@ -8,7 +8,35 @@ $(function(){
     //szerokość kontenera slideshow
     slideShow.css({'width':slideCount*100 +'%'});
     
+    //interacja po wszystkich slajdach i nadanie im marginesów oraz szerokosci
+    slideShow.find('.single-slide').each(function(index){
+        $(this).css({"width":slideWidth + '%',
+                    "margin-left": index*slideWidth + '%'});
+    });
     
+    // Przycisk poprzedni - wywolanie funkcji slider
+    $('.prev-slide').click(function(){
+        slide(slideIndex - 1);
+    });
     
+    //Przycisk następny - wywolanie funkcji slider
+    $('.next-slide').click(function(){
+        slide(slideIndex + 1);
+    });
     
-})
+    //funkcja animująca
+    function slide(newSlideIndex){
+        if(newSlideIndex < 0 || newSlideIndex >= slideCount ){
+            return;
+        }
+        var slideCaption = slider.find('.slider-caption').eq(newSlideIndex);
+        
+        slideCaption.hide();
+        var marginLeft = (newSlideIndex * (-100)) + '%';
+        slideShow.animate({"marginLeft": marginLeft}, 800, function(){
+            slideIndex = newSlideIndex;
+            slideCaption.fadeIn("slow");
+        });
+        
+    }  
+});
