a6fa7438ab9622c5c134597e015e5e5941a0b0fb
Yarikua <mr.yarikua@gmail.com>
2017-04-08 16:17:02 +0200

    Zrobiny css + część js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    var slider = $('#slider');
+    var slideShow = $('.slide-show');
+    var slideCount = $('.single-slide').length;
+    var slideWidth = 100/slideCount;
+    var slideIndex = 0;
+    
+    //szerokość kontenera slideshow
+    slideShow.css({'width':slideCount*100 +'%'});
+    
+    
+    
+    
+})
